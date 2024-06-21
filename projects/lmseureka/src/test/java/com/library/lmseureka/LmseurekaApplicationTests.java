9c5dc938bdacaefd0c65387ac56b091609212738
Benjamin <Benjaminkakai001@gmail.com>
2024-06-21 23:06:03 +0300

    Initial commit: Add WholeLibraryManagementSystem

---
+++ projects/lmseureka/src/test/java/com/library/lmseureka/LmseurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.lmseureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmseurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3ffa1a36479f51591981710950cb625be837fe86
Benjamin <Benjaminkakai001@gmail.com>
2024-06-23 12:50:09 +0300

    We need a controller, created the cotnroller and added the auth controller

---
+++ projects/user-management-service/src/main/java/com/library/management/user_management_service/security/jwt/UserDetailsImpl.java
@@ -0,0 +1,35 @@
+package com.library.management.usermanagement.security.jwt;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.library.management.usermanagement.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Objects;
+
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        // Logic to build UserDetailsImpl from User entity
+    }
+
+    // Getters and other overridden methods from UserDetails interface
+}

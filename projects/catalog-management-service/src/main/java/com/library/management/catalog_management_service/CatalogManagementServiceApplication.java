b94a2899fbf006b5a9e8fd41e5e290bb3bd268ea
Benjamin <Benjaminkakai001@gmail.com>
2024-06-24 20:28:27 +0300

    Added the catalogue management folder in the projects major folder

---
+++ projects/catalog-management-service/src/main/java/com/library/management/catalog_management_service/CatalogManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.library.management.catalog_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogManagementServiceApplication.class, args);
+	}
+
+}
---
+++ projects/catalog-management-service/src/test/java/com/library/management/catalog_management_service/CatalogManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.management.catalog_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c47f2afca9e0bd3dcb9c7e284a4ceb3faeeabf85
Benjamin <Benjaminkakai001@gmail.com>
2024-06-27 13:22:28 +0300

    Added the response class

---
+++ projects/external-catalog-integration-service/src/main/java/com/library/management/external_catalog_integration_service/client/BookResponse.java
@@ -0,0 +1,15 @@
+package com.library.management.external_catalog_integration_service.client;
+
+import java.util.List;
+
+public class BookResponse {
+    private String title;
+    private String authors;
+    private String publisher;
+    private String publishedDate;
+    private String isbn;
+    private String description;
+    private Double price;
+
+    // Getters and Setters
+}
